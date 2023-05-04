a19b48491baab17e936f8d6ab4edc48d35a0f2f3
LipCoding <sb921204@naver.com>
2023-05-04 17:56:38 +0900

    study swift start

---
+++ ios/day01.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+// 다크모드 여부
+var isDarkMode : Bool = false
+
+if (isDarkMode) {
+    print("다크모드 입니다.")
+} else {
+    print("다크모드가 아닙니다.")
+}
+
+if !isDarkMode {
+    print("다크모드가 아닙니다.")
+} else {
+    print("다크모드 입니다.")
+}
+
+
+
+var title : String = isDarkMode ? "다크모드 입니다." : "다크모드가 아닙니다."
+print("title: \(title)")
